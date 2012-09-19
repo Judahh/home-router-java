d1b8cf776d15dd8ee017de4f5e1da05d8f44f63c
judahholanda7@gmail.com <judahholanda7@gmail.com@16399f5e-af41-b19d-6f59-51f4c5daf408>
2012-09-19 23:32:42 +0000

    Nova GUI

--- HomeRouterJava/src/main/HomeRouter.java
+++ HomeRouterJava/src/main/HomeRouter.java
@@ -1,6 +1,7 @@
 package main;
 
 import view.ConnectWindow;
+import view.MainInterface;
 
 
 public class HomeRouter {
@@ -10,7 +11,9 @@ public class HomeRouter {
 			@Override
 			public void run(){
 				try {
-					new ConnectWindow();
+                                    MainInterface main = new MainInterface();
+                                    main.startInterface();
+                                    //new ConnectWindow();
 				} finally{
 					
 				}
