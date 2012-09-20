18b026c214c6d97d531f544e51154dca547cc36a
judahholanda7@gmail.com <judahholanda7@gmail.com@16399f5e-af41-b19d-6f59-51f4c5daf408>
2012-09-20 20:20:37 +0000

    Nova GUI(tem que Linkar)

--- trunk/HomeRouterJava/src/model/GUISolutionModel.java
+++ trunk/HomeRouterJava/src/model/GUISolutionModel.java
@@ -20,10 +20,46 @@ public class GUISolutionModel {
     private JLabel Ios;//Ios e versao
     private JTabbedPane Pane;
     private int PaneIndex;
-    
+
+    public GUISolutionModel(JTextArea Console, JLabel Clock, JLabel Interfaces, JLabel Type, JLabel Ios, JTabbedPane Pane, int PaneIndex) {
+        this.Console = Console;
+        this.Clock = Clock;
+        this.Interfaces = Interfaces;
+        this.Type = Type;
+        this.Ios = Ios;
+        this.Pane = Pane;
+        this.PaneIndex = PaneIndex;
+    }
     
     public void setGUIRouterName(String RouterName){
         Pane.setTitleAt(PaneIndex, RouterName);
     }
     
+    public void setGUIClock(String Clock){
+        this.Clock.setText(Clock);
+    }
+    
+    public void appendConsole(String string){
+        this.Console.append(string);
+    }
+    
+    public void appendConsole(char character){
+        this.Console.append(character+"");
+    }
+    
+    public void setGUIInterfaces(String Iterfaces){
+        this.Interfaces.setText(Iterfaces);
+    }
+    
+    public void setType(boolean Master){
+        if(Master){
+            this.Type.setText("Master");
+        }else{
+            this.Type.setText("Slave");
+        }
+    }
+    
+    public void setIos(String Ios){
+        this.Ios.setText(Ios);
+    }
 }
