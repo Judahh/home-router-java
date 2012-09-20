1cc58e76ad1fccbf18d5d90f217fc25bb141e618
judahholanda7@gmail.com <judahholanda7@gmail.com@16399f5e-af41-b19d-6f59-51f4c5daf408>
2012-09-20 19:51:49 +0000

    Nova GUI(tem que Linkar)

---
+++ HomeRouterJava/src/model/GUISolutionModel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author JH
+ */
+public class GUISolutionModel {
+    private JTextArea Console;
+    private JLabel Clock;
+    private JLabel Interfaces;
+    private JLabel Type;//Master ou Slave
+    private JLabel Ios;//Ios e versao
+}
