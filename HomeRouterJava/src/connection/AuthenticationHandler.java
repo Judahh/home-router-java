e1656ffb6b3e90f33528aedb740bd4f416e37ba5
judahholanda7@gmail.com <judahholanda7@gmail.com@16399f5e-af41-b19d-6f59-51f4c5daf408>
2012-09-21 22:39:09 +0000

    Nova GUI(tem que Linkar)

--- HomeRouterJava/src/connection/AuthenticationHandler.java
+++ HomeRouterJava/src/connection/AuthenticationHandler.java
@@ -4,116 +4,129 @@
  */
 package connection;
 
+import javax.swing.JOptionPane;
+import view.EnablePassword;
+import view.User;
+import view.VTYPassword;
+
 /**
- * 
+ *
  * @author JH
  */
 public class AuthenticationHandler {
-	private Auth auth;
-	private String passwordVTY;
-	private String passwordEnable;
-	private String user;
 
-	public AuthenticationHandler(Auth auth) {
-		this.auth = auth;
-                passwordVTY=null;
-                passwordEnable=null;
-                user=null;
-	}
+    private Auth auth;
+    private String passwordVTY;
+    private String passwordEnable;
+    private String user;
 
-	public AuthenticationHandler(int i) {
-		this.auth = Auth.values()[i];
-	}
+    public AuthenticationHandler(Auth auth) {
+        this.auth = auth;
+        passwordVTY = null;
+        passwordEnable = null;
+        user = null;
+    }
 
-	public Auth getAuthentication() {
-		return auth;
-	}
+    public AuthenticationHandler(int i) {
+        this.auth = Auth.values()[i];
+    }
 
-	public void setAuth(Auth auth) {
-		this.auth = auth;
-	}
-        
-        public String getPassword(boolean Enable) {
-            if(Enable){
-		return getPasswordB();
-            }
-            return getPassword();
-	}
+    public Auth getAuthentication() {
+        return auth;
+    }
 
-	public String getPassword() {
-            if(passwordVTY==null){
-                //abrir janela pedindo pass
-            }
-            return passwordVTY;
-	}
-        
-        public void setPassword(String password, boolean Enable) {
-            if(Enable){
-                this.passwordEnable = password;
-            }else{
-                this.passwordVTY = password;
-            }
-	}
+    public void setAuth(Auth auth) {
+        this.auth = auth;
+    }
 
-	public void setPassword(String password) {
-		this.passwordVTY = password;
-	}
+    public String getPassword(boolean Enable) {
+        if (Enable) {
+            return getPasswordB();
+        }
+        return getPassword();
+    }
 
-	public String getPasswordB() {
-            if(passwordEnable==null){
-                //abrir janela pedindo pass
-            }
-            return passwordEnable;
-	}
+    public String getPassword() {
+        if (passwordVTY == null) {
+            VTYPassword vPass=new VTYPassword();//abrir janela pedindo pass
+            passwordVTY = vPass.getPassword();
+        }
+        return passwordVTY;
+    }
 
-	public void setPasswordB(String passwordB) {
-		this.passwordEnable = passwordB;
-	}
+    public void setPassword(String password, boolean Enable) {
+        if (Enable) {
+            this.passwordEnable = password;
+        } else {
+            this.passwordVTY = password;
+        }
+    }
 
-	public String getUser() {
-		return user;
-	}
+    public void setPassword(String password) {
+        this.passwordVTY = password;
+    }
 
-	public void setUser(String user) {
-		this.user = user;
-	}
+    public String getPasswordB() {
+        if (passwordEnable == null) {
+            EnablePassword vPass=new EnablePassword();//abrir janela pedindo pass
+            passwordEnable = vPass.getPassword();
+        }
+        return passwordEnable;
+    }
 
-	public Auth getAuthenticationPrompt() {
-		return this.auth;
-	}
+    public void setPasswordB(String passwordB) {
+        this.passwordEnable = passwordB;
+    }
 
-	public Auth[] getAuthValues() {
-		return Auth.values();
-	}
+    public String getUser() {
+        if (user == null) {
+           User vUser=new User();//abrir janela pedindo pass
+           user = vUser.getUser();
+        }
+        return user;
+    }
 
-	public enum Auth {
-		Login0, Login1, Login2, Login3, Pass0, Pass1, Pass2, Pass3
-	}
+    public void setUser(String user) {
+        this.user = user;
+    }
 
-	public String getAuth() {
-		return getAuth(this.auth);
-	}
+    public Auth getAuthenticationPrompt() {
+        return this.auth;
+    }
 
-	public String getAuth(Auth auth) {
-		switch (auth) {
-		case Login0:
-			return "Login: ";
-		case Login1:
-			return "login: ";
-		case Login2:
-			return "User: ";
-		case Login3:
-			return "user: ";
-		case Pass0:
-			return "Password: ";
-		case Pass1:
-			return "password: ";
-		case Pass2:
-			return "Pass: ";
-		case Pass3:
-			return "pass: ";
-		default:
-			return "######";
-		}
-	}
+    public Auth[] getAuthValues() {
+        return Auth.values();
+    }
+
+    public enum Auth {
+
+        Login0, Login1, Login2, Login3, Pass0, Pass1, Pass2, Pass3
+    }
+
+    public String getAuth() {
+        return getAuth(this.auth);
+    }
+
+    public String getAuth(Auth auth) {
+        switch (auth) {
+            case Login0:
+                return "Login: ";
+            case Login1:
+                return "login: ";
+            case Login2:
+                return "User: ";
+            case Login3:
+                return "user: ";
+            case Pass0:
+                return "Password: ";
+            case Pass1:
+                return "password: ";
+            case Pass2:
+                return "Pass: ";
+            case Pass3:
+                return "pass: ";
+            default:
+                return "######";
+        }
+    }
 }
